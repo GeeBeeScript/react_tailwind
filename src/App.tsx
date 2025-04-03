4bf45dbced85c482124d917c83af73ca2a6a9289
GeeBeeScript <gbemigashodimu@gmail.com>
2025-04-02 22:19:48 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <p className="border-2 border-tahiti-400 bg-happy-yellow">Tailwind Css</p>
+        <p className="text-xs sm:text-base md:text-lg lg:text-3xl">Tailwind Css</p>
+        <button className='bg-sky-500 hover:bg-sky-700'>Styled button</button>
+      </div>
+    </>
+  )
+}
+
+export default App
