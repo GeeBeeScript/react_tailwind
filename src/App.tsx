da31a6ef276a5b6152d44432b19783513e5a639e
GeeBeeScript <gbemigashodimu@gmail.com>
2025-04-03 13:31:57 -0700

    add some typography styles

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,15 @@ function App() {
         <p className="border-2 border-tahiti-400 bg-happy-yellow">Tailwind Css</p>
         <p className="text-xs sm:text-base md:text-lg lg:text-3xl">Tailwind Css</p>
         <button className='bg-sky-500 hover:bg-sky-700'>Styled button</button>
+        {/* Typography */}
+        <h1 className='text-[20px]'>First text</h1>
+        <h2 className=''>Second text</h2>
+        <h3 className='text-xl'>Third text</h3>
+        <p className='text-base font-winky'>Normal text - regular text size</p>
+        <p className='text-xs'>Small text</p>
+        <p className='text-sm'>Slightly larger, but small text</p>
+        <p className='italic font-light underline decoration-blue-400 decoration-wavy decoration-4 underline-offset-6 leading-loose'>New random text</p>
+        <p className='capitalize'>new line</p>
       </div>
     </>
   )
